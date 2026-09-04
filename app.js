7ea2d50452b8b71939c1dfc9e02745d7bb76ec3b
technovahub2025 <technovahubcareer@gmail.com>
2026-09-04 12:29:49 +0530

    feat: enhance helmet middleware configuration for improved security

--- app.js
+++ app.js
@@ -43,8 +43,14 @@ app.use(
     ],
   })
 );
+app.use(
+  helmet({
+    crossOriginResourcePolicy: {
+      policy: 'cross-origin',
+    },
+  })
+);
 
-app.use(helmet());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
